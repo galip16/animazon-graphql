433aa64d9347ce0fe615c30501beb387bba72c7f
galip16 <galipsvt16@gmail.com>
2021-12-14 22:46:54 +0100

    category type and resolvers done

--- server/index.js
+++ server/index.js
@@ -1,38 +1,69 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { mainCards, animals, categories } = require("./db.js")
 
 
 const typeDefs = gql`
 
-  type Book {
-    title: String
-    author: String
+
+
+
+  type MainCard {
+    title : String!
+    image : String!
+  }
+
+  type Animal{
+    id: ID!
+    image: String!
+    title: String!
+    rating: Float
+    price:String!
+    description:[String!]!
+    slug: String!
+    stock: Int!
+    onSale:Boolean
+
+  }
+
+  type Category {
+    id: ID!
+    image: String!
+    category: String!
+    slug: String!
   }
 
  
   type Query {
-    books: [Book]
+   
+    mainCards:[MainCard]
+    animals: [Animal]
+    animal(slug:String!): Animal
+    categories: [Category!]!
+    category(slug: String!) : Category
   }
-`;
+`
 
 
-
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
 
 
 
 const resolvers = {
-    Query: {
-        books: () => books,
+  Query: {
+    mainCards: () => mainCards,
+    animals: () => animals,
+    animal: (parent, args, context) => {
+      let animal = animals.find((animal) => animal.slug === args.slug)
+      return animal
     },
+    categories: () => categories,
+    category: (parent, args, context) => {
+      let category = categories.find((category) => {
+        return category.slug === args.slug
+      });
+      return category
+    }
+
+  },
 };
 
 
@@ -40,5 +71,5 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 
 server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`);
 });
