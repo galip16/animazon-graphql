282cfbd7d2c4eb455d08ef0e8acc27ca3dfbf016
galip16 <galipsvt16@gmail.com>
2021-12-15 18:23:35 +0100

    mutation added

--- server/index.js
+++ server/index.js
@@ -1,75 +1,27 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { mainCards, animals, categories } = require("./db.js")
+const { ApolloServer } = require('apollo-server');
+const { mainCards, animals, categories } = require('./db')
+const typeDefs = require('./schema')
+const Query = require("./resolvers/Query")
+const Mutation = require("./resolvers/Mutation")
+const Animal = require("./resolvers/Animal")
+const Category = require("./resolvers/Category")
 
-
-const typeDefs = gql`
-
-
-
-
-  type MainCard {
-    title : String!
-    image : String!
-  }
-
-  type Animal{
-    id: ID!
-    image: String!
-    title: String!
-    rating: Float
-    price:String!
-    description:[String!]!
-    slug: String!
-    stock: Int!
-    onSale:Boolean
-
-  }
-
-  type Category {
-    id: ID!
-    image: String!
-    category: String!
-    slug: String!
-  }
-
- 
-  type Query {
-   
-    mainCards:[MainCard]
-    animals: [Animal]
-    animal(slug:String!): Animal
-    categories: [Category!]!
-    category(slug: String!) : Category
-  }
-`
-
-
-
-
-
-const resolvers = {
-  Query: {
-    mainCards: () => mainCards,
-    animals: () => animals,
-    animal: (parent, args, context) => {
-      let animal = animals.find((animal) => animal.slug === args.slug)
-      return animal
-    },
-    categories: () => categories,
-    category: (parent, args, context) => {
-      let category = categories.find((category) => {
-        return category.slug === args.slug
-      });
-      return category
-    }
-
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    Mutation,
+    Animal,
+    Category
   },
-};
+  context: {
+    mainCards,
+    animals,
+    categories
+  }
+});
 
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-
+// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
